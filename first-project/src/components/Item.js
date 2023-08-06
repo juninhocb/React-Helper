6681c26021d2821e4380acff7eeacdd4a2e5c87f
juninhocb <juninhocb@homail.com>
2023-08-06 00:49:43 -0300

    propTypes

--- first-project/src/components/Item.js
+++ first-project/src/components/Item.js
@@ -1,9 +1,22 @@
-function Item (props){
+import PropTypes from 'prop-types'
+
+function Item ({team, titles}){
     return (
         <>
-            <li>{props.team}</li>
+            <li>{team} - {titles}</li>
         </>
     )
+}
+
+//required does not make sense with default props
+Item.propTypes = {
+    team: PropTypes.string.isRequired, 
+    titles: PropTypes.number.isRequired
+}
+
+Item.defaultProps = {
+    marca: 'Title need to be set',
+    titles: 0
 }
 
 export default Item
--- first-project/src/components/ListOfTeams.js
+++ first-project/src/components/ListOfTeams.js
@@ -5,9 +5,10 @@ function List(){
         <>
             <h1> My List of teams </h1>
             <ul>
-                <Item team="palmeiras"/>
-                <Item  team="atlético-mg"/>
-                <Item team="grêmio"/>
+                <Item team="palmeiras" titles={23} />
+                <Item  team="atlético-mg" titles={10}/>
+                <Item team="grêmio" titles={17}/>
+                <Item />
             </ul>
         </>
     )
