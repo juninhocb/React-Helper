39aefc0307f3ac6a279ed870fa1149b5c0b5e0ee
juninhocb <juninhocb@homail.com>
2023-08-06 15:00:13 -0300

    icons

--- first-project/src/components/layout/Footer.js
+++ first-project/src/components/layout/Footer.js
@@ -1,7 +1,16 @@
+import {FaFacebook, FaInstagram, FaLinkedin} from 'react-icons/fa'
+import styles from './Footer.module.css'
+
 function Footer(){
     return (
         <>
-            <footer> Carlos jr - Creator</footer>
+            <footer> 
+                <ul className={styles.socialList}>
+                    <li className={styles.item} ><FaLinkedin/></li>
+                    <li className={styles.item}><FaInstagram/></li>
+                    <li className={styles.item} ><FaFacebook/></li>
+                </ul>
+            </footer>
         </>
     )
 }
